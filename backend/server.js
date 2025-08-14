13db10af1074030b970ba233804b854e04d5a1fb
prasant-evoquesys <prasantevoquesys@gmail.com>
2025-08-14 12:11:32 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,8 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-// import app from "./app.js";
-// import sequelize from "./config/db.js";
-
-
-// const PORT = process.env.PORT || 8000;
-
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("✅ MySQL connected");
-
-//     await sequelize.sync(); // In production, use migrations
-//     console.log("✅ All models synchronized");
-
-//     app.listen(PORT, () => {
-//       console.log(`🚀 Server running on ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("❌ DB connection failed:", error);
-//   }
-// })();
-
-
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
 import sequelize from "./config/db.js";
+
 
 const PORT = process.env.PORT || 8000;
 
@@ -38,10 +14,11 @@ const PORT = process.env.PORT || 8000;
     await sequelize.sync(); // In production, use migrations
     console.log("✅ All models synchronized");
 
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on ${PORT}`);
     });
   } catch (error) {
     console.error("❌ DB connection failed:", error);
   }
 })();
+
